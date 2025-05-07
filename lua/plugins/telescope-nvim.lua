4be3f22a2e8ef05ed395dc53eb43254d9b39393b
ThatStevenGuy <65724470+ThatStevenGuy@users.noreply.github.com>
2025-05-07 21:35:34 +0200

    Fixed: trouble.nvim integration with telescope errors out.

--- lua/plugins/telescope-nvim.lua
+++ lua/plugins/telescope-nvim.lua
@@ -6,12 +6,12 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        local trouble = require("trouble.providers.telescope")
+        local trouble = require("trouble.sources.telescope")
         require("telescope").setup {
             defaults = {
                 mappings = {
-                    i = { ["<c-t>"] = trouble.open_with_trouble },
-                    n = { ["<c-t>"] = trouble.open_with_trouble },
+                    i = { ["<c-t>"] = trouble.open },
+                    n = { ["<c-t>"] = trouble.open },
                 },
             },
             pickers = {

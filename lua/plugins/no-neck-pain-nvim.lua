73f10a86b6d2e349414a7de5b3926c85f2812ff5
ThatStevenGuy <65724470+ThatStevenGuy@users.noreply.github.com>
2025-07-18 20:06:39 +0200

    Added a config for no-neck-pain.nvim

---
+++ lua/plugins/no-neck-pain-nvim.lua
@@ -0,0 +1,15 @@
+return {
+    "shortcuts/no-neck-pain.nvim",
+    opts = {
+        width = 130,
+        -- autocmds = {
+        --     enableOnVimEnter = true
+        -- },
+        integrations = {
+            NvimTree = {
+                position = "left",
+                reopen = true
+            }
+        }
+    }
+}

9cd860adf68ac8dadb30ea23ba5b3fe2b2d0846c
ThatStevenGuy <65724470+ThatStevenGuy@users.noreply.github.com>
2025-07-18 20:06:06 +0200

    Removed broken DiagnosticSign overrides.

--- lua/plugins/trouble-nvim.lua
+++ lua/plugins/trouble-nvim.lua
@@ -1,16 +1,5 @@
 return {
     "folke/trouble.nvim", -- Search results & diagnostics window
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        -- Set Trouble's signs as the LSP diagnostic signs
-        function firstToUpper(str)
-            return (str:gsub("^%l", string.upper))
-        end
-
-        local signs = require("trouble.config").options.signs
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. firstToUpper(type)
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-         end
-    end
+    cmd = "Trouble"
 }
